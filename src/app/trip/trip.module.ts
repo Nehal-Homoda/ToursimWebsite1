4163143c14e9455bfeaf38c1f0e51eccbf17d179
Jamal40 <Jamal_Ali@outlook.com>
2023-06-23 01:20:03 +0300

    Adding Missing Parts

--- src/app/trip/trip.module.ts
+++ src/app/trip/trip.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TripsComponent } from './trips/trips.component';
 import { CardModule } from 'primeng/card';
-
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
-  declarations: [
-    TripsComponent
-  ],
-  imports: [
-    CommonModule,CardModule
-  ]
+  declarations: [TripsComponent],
+  imports: [CommonModule, CardModule, ButtonModule],
 })
-export class TripModule { }
+export class TripModule {}
