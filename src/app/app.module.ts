e419d6185616605e830481d5495f7dee15706134
Jamal40 <Jamal_Ali@outlook.com>
2023-06-22 23:56:57 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,11 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
-//import { AgencyComponent } from './agency/agency/agency.component';
-//import { AppRoutingModule } from './app-routing/app-routing.module';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
@@ -17,7 +14,6 @@ import { TripModule } from './trip/trip.module';
 import { RatingModule } from 'primeng/rating';
 import { ButtonModule } from 'primeng/button';
 import { AgencyModule } from './agency/agency.module';
-//import { AgencyDetailsComponent } from './agency-details/agency-details.component';
 
 @NgModule({
   declarations: [
@@ -25,9 +21,6 @@ import { AgencyModule } from './agency/agency.module';
     HomeComponent,
     AboutUsComponent,
     ContactUsComponent,
-    AgencyComponent,
-   // AgencyDetailsComponent,
-    
   ],
   imports: [
     BrowserModule,
@@ -39,7 +32,7 @@ import { AgencyModule } from './agency/agency.module';
     FormsModule,
     RatingModule,
     ButtonModule,
-    AgencyModule
+    AgencyModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
